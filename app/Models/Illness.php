be8c98be9ae5cd15d79a934e8b1f8a77ae29dcc7
AhmedMeshaal <ahmedmeshaal.66@gmail.com>
2025-05-29 13:32:34 +0300

    ContactType + Illness Modules

---
+++ app/Models/Illness.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Illness extends Model
+{
+    public function resolveRouteBinding($value, $field = null)
+    {
+        return $this->where($field ?? 'id', $value)->withTrashed()->firstOrFail();
+    }
+
+    public function lesion(): BelongsTo
+    {
+        return $this->belongsTo(Lesion::class);
+    }
+}
